b05fc7225830dea12dee6f28b8595f7fdae03c13
luc.samain2 <luc.samain2@gmail.com>
2025-03-24 21:30:20 +0100

    Initial commit

---
+++ luc.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+
+sample_df = pd.read_csv('insa-ml-2025-regression/sample_submission.csv')
+train_df = pd.read_csv('insa-ml-2025-regression/train.csv')
+test_df = pd.read_csv('insa-ml-2025-regression/test.csv')
+
+
